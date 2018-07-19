661d2f5985903d931de4a91738f7b26a547b111a
Benjamin Catouillard <catouillard.benjamin@gmail.com>
2018-07-19 16:44:21 +0200

    Feat

--- AhnacSolutions/MainPage.xaml.cs
+++ AhnacSolutions/MainPage.xaml.cs
@@ -95,6 +95,7 @@ namespace AhnacSolutions
         {
             if (ApiInformation.IsTypePresent("Windows.UI.StartScreen.StartScreenManager"))
             {
+                StartupTask startupTask = await StartupTask.GetAsync("AhnacSolutions");
                 var entry = (await Package.Current.GetAppListEntriesAsync())[0];
 
                 var startScreenManager = StartScreenManager.GetDefault();
